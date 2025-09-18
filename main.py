bf7dbbae3de55674ebd4051b6a77d29c8c98fc68
giovannialayan <giovannialayan@gmail.com>
2025-09-18 14:20:34 -0400

    added monsters and added name to the data and changed key to be name without spaces

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from fastapi.staticfiles import StaticFiles
-from itemDict import items, weapons, shields, fieldMagics, offensiveMagics, defensiveMagics
+from itemDict import items, weapons, shields, fieldMagics, offensiveMagics, defensiveMagics, monsters
 
 app = FastAPI(title="page")
 app_api = FastAPI(title="api")
@@ -9,17 +9,28 @@ app.mount("/tool", app_api)
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
-@app_api.get("/{itemName}")
-async def get_item(itemName: str):
+@app_api.get("/item/{itemName}")
+async def get_item(itemName: str, response: Response):
     if (itemName in items):
-        return {"data": items[itemName], "name": itemName, "type": 0}
+        return {"data": items[itemName], "type": 0}
     elif (itemName in weapons):
-        return {"data": weapons[itemName], "name": itemName, "type": 1}
+        return {"data": weapons[itemName], "type": 1}
     elif (itemName in shields):
-        return {"data": shields[itemName], "name": itemName, "type": 2}
+        return {"data": shields[itemName], "type": 2}
     elif (itemName in offensiveMagics):
-        return {"data": offensiveMagics[itemName], "name": itemName, "type": 3}
+        return {"data": offensiveMagics[itemName], "type": 3}
     elif (itemName in defensiveMagics):
-        return {"data": defensiveMagics[itemName], "name": itemName, "type": 4}
+        return {"data": defensiveMagics[itemName], "type": 4}
     elif (itemName in fieldMagics):
-        return {"data": fieldMagics[itemName], "name": itemName, "type": 5}
+        return {"data": fieldMagics[itemName], "type": 5}
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"error": "item not found"}
+    
+@app_api.get("/monster/{monsterName}")
+async def get_monster(monsterName: str, response: Response):
+    if (monsterName in monsters):
+        return {"data": monsters[monsters]}
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"error": "monster not found"}
